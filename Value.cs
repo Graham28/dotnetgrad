bc4acacc8bde4377b99efceacabc8ba97c9bd57a
Graham Phillips <graham@Grahams-MacBook-Pro.local>
2022-12-31 13:33:25 +0000

    add derivative function to value.cs

--- Value.cs
+++ Value.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace dotnetgrad
 {
 	public class Value
@@ -7,47 +9,72 @@ namespace dotnetgrad
 		public List<Value> Children { get; }
 		public Operation Operation { get; }
 		public double Gradient { get; set; }
+        private Func<Value, double> _derivative;
 
-		public Value(double data, List<Value>? children = null,
-			Operation operation = Operation.None, double gradient = 0)
+
+        public Value(double data,
+			List<Value>? children = null,
+			Operation operation = Operation.None,
+            Func<Value, double>? derivative = null,
+            double gradient = 0)
 		{
 			Data = data;
 			Children = children ?? new List<Value>();
 			Operation = operation;
-			Gradient = gradient; 
+			Gradient = gradient;
+            _derivative = derivative ?? (child => 1);
 		}
 
         public Value Add(Value otherValue)
         {
-			return new Value(Data + otherValue.Data, new List<Value>() { this, otherValue }, Operation.Add);
+			return new Value(Data + otherValue.Data, new List<Value>() { this, otherValue }, Operation.Add, child => 1);
         }
 
         public Value Multiply(Value otherValue)
         {
-            return new Value(Data * otherValue.Data, new List<Value>() { this, otherValue }, Operation.Multiply);
+            var thisData = Data;
+            return new Value(Data * otherValue.Data,
+                new List<Value>() { this, otherValue },
+                Operation.Multiply,
+                child => thisData != child.Data ? thisData : otherValue.Data);
         }
+
         public Value TanH()
         {
 			var n = Data;
 			var tanH = (Math.Exp(2*n) - 1) / (Math.Exp(2*n) + 1);
-            return new Value(tanH, new List<Value>() { this }, Operation.TanH);
+            return new Value(tanH, new List<Value>() { this }, Operation.TanH, child => 1 - Math.Pow(tanH, 2));
         }
 
 		public void Backword()
 		{
+            var topologicallyOrderedListOfNodes = orderNodesTopologically(this, new HashSet<Value>(), new List<Value>());
+            foreach (var node in topologicallyOrderedListOfNodes)
+            {
+                node.SetChildGradients();
+            }
+        }
+
+        public void SetChildGradients()
+        {
             foreach (var child in Children)
             {
-				if (this.Operation == Operation.Multiply)
-				{
-                    child.Gradient += Children.Where(x => !x.Equals(child)).First().Data * Gradient;
-                }
-				else
-				{
-                    child.Gradient += Gradient;
-                }
-                child.Backword();
+                var d = _derivative(child);
+                child.Gradient += d * Gradient;
             }
+        }
 
+        private List<Value> orderNodesTopologically(Value node, HashSet<Value> visited, List<Value> returnList)
+        {
+            if (!visited.Contains(node))
+                visited.Add(node);
+            foreach (var child in node.Children)
+            {
+                orderNodesTopologically(child, visited, returnList);
+            }
+            returnList.Add(node);
+            returnList.Reverse(); 
+            return returnList;
         }
     }
 }
