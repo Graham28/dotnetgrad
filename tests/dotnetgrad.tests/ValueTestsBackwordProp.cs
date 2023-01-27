8b83860509151086cd2b448ff386a0bd3c6d8282
Graham Phillips <graham@grahams-mbp.home>
2023-01-27 22:00:08 +0000

    Clean up

---
+++ tests/dotnetgrad.tests/ValueTestsBackwordProp.cs
@@ -0,0 +1,127 @@
+﻿using System;
+namespace dotnetgrad.tests
+{
+	public class ValueTestsBackwordProp
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Backword_Addition()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+            var firstPlusSecond = firstValue.Add(secondValue);
+            firstPlusSecond.Gradient = 0.5;
+
+            //Act
+            firstPlusSecond.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient.Equals(0.5));
+            Assert.That(secondValue.Gradient.Equals(0.5));
+        }
+
+        [Test]
+        public void Backword_Subtract()
+        {
+            //Arrange
+            var firstValue = new Value(3.0);
+            var secondValue = new Value(2.0);
+            var firstMinusSecond = firstValue.Subtract(secondValue);
+            firstMinusSecond.Gradient = 0.5;
+
+            //Act
+            firstMinusSecond.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient.Equals(0.5));
+            Assert.That(secondValue.Gradient.Equals(0.5));
+        }
+
+        [Test]
+        public void Backword_Multiply()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+            var firstTimesSecond = firstValue.Multiply(secondValue);
+            firstTimesSecond.Gradient = 1.0;
+
+            //Act
+            firstTimesSecond.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient.Equals(3.0));
+            Assert.That(secondValue.Gradient.Equals(2.0));
+        }
+
+        [Test]
+        public void Backword_Pow()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+            var firstToThePowerSecond = firstValue.Pow(secondValue);
+            firstToThePowerSecond.Gradient = 1.0;
+
+            //Act
+            firstToThePowerSecond.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient, Is.EqualTo(12.0));
+            Assert.That(secondValue.Gradient, Is.EqualTo(Math.Pow(firstValue.Data, secondValue.Data)*Math.Log(firstValue.Data)));
+        }
+
+        [Test]
+        public void Backword_Devide()
+        {
+            //Arrange
+            var firstValue = new Value(6.0);
+            var secondValue = new Value(2.0);
+            var firstToThePowerSecond = firstValue.Devide(secondValue);
+            firstToThePowerSecond.Gradient = 1.0;
+
+            //Act
+            firstToThePowerSecond.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient, Is.EqualTo(0.5));
+            Assert.That(secondValue.Gradient, Is.EqualTo(-1.5));
+        }
+
+        [Test]
+        public void Backword_Exp()
+        {
+            //Arrange
+            var firstValue = new Value(0.5);
+            var expOfFirstValue = firstValue.Exp();
+            expOfFirstValue.Gradient = 1.0;
+
+            //Act
+            expOfFirstValue.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient, Is.EqualTo(Math.Exp(0.5)).Within(0.0001));
+        }
+
+        [Test]
+        public void Backword_TanH()
+        {
+            //Arrange
+            var firstValue = new Value(0.5);
+            var tanHOfFirstValue = firstValue.TanH();
+            tanHOfFirstValue.Gradient = 1.0;
+
+            //Act
+            tanHOfFirstValue.Backword();
+
+            //Assert
+            Assert.That(firstValue.Gradient, Is.EqualTo(0.7864).Within(0.0001));
+        }
+    }
+}
+
---
+++ tests/dotnetgrad.tests/ValueTestsForwardProp.cs
@@ -0,0 +1,115 @@
+﻿using System;
+namespace dotnetgrad.tests
+{
+	public class ValueTestsForwardProp
+	{
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Add()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+
+            //Act
+            var firstPlusSecond = firstValue.Add(secondValue);
+            var secondPlusFirst = secondValue.Add(firstValue);
+            //Assert
+            Assert.That(firstPlusSecond.Data.Equals(5));
+            Assert.That(secondPlusFirst.Data.Equals(5));
+        }
+
+        [Test]
+        public void Multiply()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+
+            //Act
+            var firstPlusSecond = firstValue.Multiply(secondValue);
+            var secondPlusFirst = secondValue.Multiply(firstValue);
+
+            //Assert
+            Assert.That(firstPlusSecond.Data.Equals(6));
+            Assert.That(secondPlusFirst.Data.Equals(6));
+        }
+
+        [Test]
+        public void Exp()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+
+            //Act
+            var expFirstValue = firstValue.Exp();
+            //Assert
+            Assert.That(expFirstValue.Data, Is.EqualTo(7.389).Within(0.01));
+        }
+
+        [Test]
+        public void ToThePowerOf()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+
+            //Act
+            var firstValueToThePowerOfSecondValue = firstValue.Pow(secondValue);
+            //Assert
+            Assert.That(firstValueToThePowerOfSecondValue.Data, Is.EqualTo(8.0));
+        }
+
+        [Test]
+        public void Devide()
+        {
+            //Arrange
+            var firstValue = new Value(6.0);
+            var secondValue = new Value(2.0);
+
+            //Act
+            var firstValueDevidedBySecondValue = firstValue.Devide(secondValue);
+            //Assert
+            Assert.That(firstValueDevidedBySecondValue.Data, Is.EqualTo(3.0));
+        }
+
+        [Test]
+        public void Children()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+
+            //Act
+            var firstPlusSecond = firstValue.Add(secondValue);
+            var secondPlusFirst = secondValue.Multiply(firstValue);
+
+            //Assert
+            Assert.That(firstPlusSecond.Children.Contains(firstValue));
+            Assert.That(secondPlusFirst.Children.Contains(firstValue));
+            Assert.That(firstPlusSecond.Children.Contains(secondValue));
+            Assert.That(secondPlusFirst.Children.Contains(secondValue));
+        }
+
+        [Test]
+        public void Opertion()
+        {
+            //Arrange
+            var firstValue = new Value(2.0);
+            var secondValue = new Value(3.0);
+
+            //Act
+            var firstPlusSecond = firstValue.Add(secondValue);
+            var secondPlusFirst = secondValue.Multiply(firstValue);
+
+            //Assert
+            Assert.That(firstPlusSecond.Operation.Equals(Operation.Add));
+            Assert.That(secondPlusFirst.Operation.Equals(Operation.Multiply));
+        }
+    }
+}
+
